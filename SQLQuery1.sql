d3d9638cbd1904f2ff0c4643df6e3dfd3fc697d8
chunChu <doanthanhchung03nb@gmail.com>
2024-04-13 15:39:51 +0700

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,9 +1,10 @@
-use InterviewSystem
+﻿use InterviewSystem
 
 select * from UserRole
 select * from UserStatus
 select * from [User]
 select * from Department
 UPDATE [User] SET Usename = 'Huynv1', FullName = '' WHERE UserID = 1;
-
+delete from [User] where UserID = 31
 select * from Department
+select * from [User] where FullName like '%chung%' and UserRoleID = 1
