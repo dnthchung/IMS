19cbd436a66e2f7f73c3a31eff5b7659e7a6b6ca
chunChu <doanthanhchung03nb@gmail.com>
2024-04-14 10:02:34 +0700

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -8,3 +8,4 @@ UPDATE [User] SET Usename = 'Huynv1', FullName = '' WHERE UserID = 1;
 delete from [User] where UserID = 31
 select * from Department
 select * from [User] where FullName like '%chung%' and UserRoleID = 1
+UPDATE [User] SET UserStatusID = 1 WHERE UserID = 1;
